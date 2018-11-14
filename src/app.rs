aa66343a3d4383851a1b2be97d09d441798e6da4
KeisukeYamashita <19yamashita15@gmail.com>
2018-11-15 02:31:59 +0900

    :paw_prints: add subcommand but arg not working

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,54 @@
 extern crate clap;
-use clap::App;
+
+use clap::{App, Arg, SubCommand};
+
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 fn main() {
-  App::new("vasp-utils")
-                    .version("v1.0-beta")
-                    .author("KeisukeYamashita <19yamashita15@gmail.com>")
-                    .about("A useful VASP tools to help your research accelerate")
-                    .get_matches();
-  println!("hoeg");
+    let matches = App::new("vasp-utils")
+        .version("v1.0.0-beta")
+        .author("KeisukeYamashita <19yamashita15@gmail.com>")
+        .about("A useful VASP tools to help your research accelerate")
+        .args(&[Arg::with_name("debug")
+            .help("turn on debug")
+            .short("v")
+            .long("debug")]).subcommand(
+            SubCommand::with_name("format")
+                .about("format input file of vasp into specific type of file")
+                .arg(
+                    Arg::with_name("input")
+                        .help("the input file to format")
+                        .short("i")
+                        .long("input")
+                        .required(true),
+                ).arg(
+                    Arg::with_name("output")
+                        .help("specify output file and path")
+                        .short("o")
+                        .long("output"),
+                ),
+        ).get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("format") {
+        if let Some(i) = matches.value_of("input") {
+            println!("{}", i)
+        }
+        println!("Adding file: {}", matches.value_of("input").unwrap());
+        // let path = Path::new(input_file_path);
+        // let display = path.display();
+
+        // let mut file = match File::open(input_file_path) {
+        //   Err(why) => panic!("couldn`t find {}: {}", display, why.description()),
+        //   Ok(file) => file,
+        // };
+
+        // let mut s = String::new();
+        // match file.read_to_string(&mut s) {
+        //   Err(why) => panic!("couldn`t read {}: {}",display, why.description()),
+        //   Ok(_) => println!("{}", s)
+        // };
+    }
 }
