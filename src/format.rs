1c626b43f37295b1b142a26169c700595fc3b94d
KeisukeYamashita <19yamashita15@gmail.com>
2018-11-17 22:43:39 +0900

    :zap: add formatter

--- src/format.rs
+++ src/format.rs
@@ -1,9 +1,23 @@
-pub struct Formatter {
+use std::path::Path;
+
+/// `Formatter` represents broker for user and  this command line interface.
+/// 
+/// This is like a handler, if you are familier with web application. It contains informations such as
+/// `input file`, `output file` and the `action` from the user input.
+pub struct Formatter<'a> {
+    /// `input` represents field for user input file path.
+    pub input: &'a Path,
+
+    // `output` represent field for user output file path.
+    pub output: &'a Path
 }
 
-impl Formatter {
-    pub fn new() -> Self {
+impl<'a> Formatter<'a> {
+    /// `new()` initializes the `Formatter` struct with user input.
+    pub fn new(input_path: &'a Path, output_path: &'a Path) -> Self {
         Formatter {
+            input: input_path,
+            output: output_path
         }
     }
 }
